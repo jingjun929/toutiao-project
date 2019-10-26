d6460139baed7d806e7e9bde91d5b1c72c816048
jingjun929 <348443829@qq.com>
2019-10-26 19:41:03 +0800

    登录静态完成

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="app">
-    hm-toutiao-pc-85
-    <el-button type="success">成功按钮</el-button>
+    <!-- 一级路由的容器 -->
+    <router-view />
   </div>
 </template>
+
 <script>
 export default {
   name: 'app'
 }
 </script>
 
-<style>
-</style>
+<style lang="less"></style>
